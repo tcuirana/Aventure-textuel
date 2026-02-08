3e2256081ba97b84b49c21958ff79548b1a9f3b2
tcuirana <tcuirana@gaming.tech>
2026-02-08 10:46:24 +0100

    ClassChoice

--- main.cpp
+++ main.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
 
+int main()
+{
+	std::cout << "Bienvenue dans le donjon !" << std::endl;
+
+	int classChoice;
+	std::cout << "Choisir une classe : Guerrier(1), Mage(2), Archer(3)";
+	std::cin >> classChoice;
+
+	switch (classChoice)
+	{
+	case(1):
+		std::cout << "Vous avez choisi le Guerrier." << std::endl;
+		break;
+	case(2):
+		std::cout << "Vous avez choisi le Mage." << std::endl;
+		break;
+	case(3):
+		std::cout << "Vous avez choisi l Archer." << std::endl;
+		break;
+	}
+
+
+
+	return 0;
+}
