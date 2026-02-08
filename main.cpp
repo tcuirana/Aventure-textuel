3ec777f82f32de78608b9641a37344a786d79e3d
tcuirana <tcuirana@gaming.tech>
2026-02-08 12:07:26 +0100

    Stats Display + clean terminal

--- main.cpp
+++ main.cpp
@@ -1,51 +1,64 @@
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
-	std::cout << "Bienvenue dans le donjon !" << std::endl;
+	std::cout << "Bienvenue dans le donjon !" << std::endl << std::endl;
 
 	//Choose a class
 	int classChoice;
-	std::cout << "Choisir une classe : Guerrier(1), Mage(2), Archer(3)";
+	std::cout << "Choisir une classe : Guerrier (1), Mage (2), Archer (3)";
 	std::cin >> classChoice;
 
+	//Choice display
 	switch (classChoice)
 	{
 	case(1):
-		std::cout << "Vous avez choisi le Guerrier." << std::endl;
+		std::cout << "Vous avez choisi le Guerrier." << std::endl << std::endl;
 		break;
 	case(2):
-		std::cout << "Vous avez choisi le Mage." << std::endl;
+		std::cout << "Vous avez choisi le Mage." << std::endl << std::endl;
 		break;
 	case(3):
-		std::cout << "Vous avez choisi l Archer." << std::endl;
+		std::cout << "Vous avez choisi l Archer." << std::endl << std::endl;
 		break;
 	}
 
-	//Stats Initialization
+	//Stats initialization
+	int hp = 0;
+	int atk = 0;
+	int dfs = 0;
+
 	if (classChoice == 1)
 	{
-		//Warrior Stats
-		int hp = 200;
-		int atk = 30;
-		int dfs = 15;
+		//Warrior stats
+		hp = 200;
+		atk = 20;
+		dfs = 15;
 	}
 	else if (classChoice == 2)
 	{
-		//Wizard Stats
-		int hp = 120;
-		int atk = 5;
-		int dfs = 5;
+		//Wizard stats
+		hp = 120;
+		atk = 25;
+		dfs = 5;
 	}
 	else if (classChoice == 3)
 	{
-		//Bowman Stats
-		int hp = 120;
-		int atk = 5;
-		int dfs = 5;
+		//Bowman stats
+		hp = 150;
+		atk = 20;
+		dfs = 10;
 	}
 
+	//Stats display
+	std::cout << "Vos statistiques :" << std::endl;
+	std::cout << "Hp : " << hp << std::endl;
+	std::cout << "Atk : " << atk << std::endl;
+	std::cout << "Dfs : " << dfs << std::endl;
 
+	Sleep(4000);
+	system("cls");
 
 	return 0;
 }
