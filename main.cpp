c49eacdee9eb81604cdd49cb7d59664f348e44a7
tcuirana <tcuirana@gaming.tech>
2026-02-10 00:05:48 +0100

    Update main.cpp
    Goblin Init + Action choice

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int main()
 		std::cout << "Vous avez choisi l Archer." << std::endl << std::endl;
 		break;
 	}
+	
 
 	//Stats initialization
 	int hp = 0;
@@ -55,10 +56,57 @@ int main()
 	std::cout << "Vos statistiques :" << std::endl;
 	std::cout << "Hp : " << hp << std::endl;
 	std::cout << "Atk : " << atk << std::endl;
-	std::cout << "Dfs : " << dfs << std::endl;
+	std::cout << "Dfs : " << dfs << std::endl << std::endl;
 
-	Sleep(4000);
-	system("cls");
+	//Sleep(4000);
+	//system("cls");
+
+	std::cout << "Un Gobelin apparait." << std::endl << std::endl;
+	std::cout << "Statistiques du gobelin :" << std::endl;
+	
+	//Goblin Stats
+	int gobHp = 180;
+	int gobAtk = 35;
+	int gobDfs = 8;
+
+	//Display Goblin Stats
+	std::cout << "Hp : " << gobHp << std::endl;
+	std::cout << "Atk : " << gobAtk << std::endl;
+	std::cout << "Dfs : " << gobDfs << std::endl << std::endl;
+
+	//Action
+	std::cout << "Que voulez vous faire ?" << std::endl;
+	std::cout << "1 :  Attaquer" << std::endl;
+	std::cout << "2 : Se soigner" << std::endl;
+	int choice;
+	std::cin >> choice;
+
+	if (choice == 1)
+	{
+		int dmg = atk - gobDfs;
+		
+			if (dmg < 1)
+			{	
+				dmg = 1;
+			}
+
+		gobHp -= dmg;
+	}
+	else if (choice == 2 )
+	{
+		if (hp <= 150 || hp == 150)
+		{
+			hp += 50;
+		}
+		else if (hp >= 150)
+		{
+			hp = 200;
+		}
+	}
+	else if (hp == 200)
+	{
+		std::cout << "Vous avez toute votre vie !" << std::endl << std::endl;
+	}
 
 	return 0;
 }
